44324292bed8a8a2bdbd39d83235362ff49a3fa1
SoumenCodes <soumen19j@gmail.com>
2024-03-22 23:25:09 +0530

    social icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import { useState } from "react";
 import "./App.css";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <></>;
+  return (
+    <div className="m-10">
+      <Navbar />
+      <Home />
+    </div>
+  );
 }
 
 export default App;
